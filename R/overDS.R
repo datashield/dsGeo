5d7b4449f4841f78eee2da7d133d1a48b78c5518
tombisho <trpb2@cantab.net>
2015-10-23 14:23:12 +0100

    Changed the overDS function to make it more generic. Now the whole list can be returned and a function specified
    Signed-off-by: tombisho <trpb2@cantab.net>

--- R/overDS.R
+++ R/overDS.R
@@ -10,19 +10,27 @@
 #' @details See the over() function from sp and rgeos package for more details
 #' @param x geometry (locations) of the queries
 #' @param y layer from which the geometries or attributes are queried
-#' @return A vector of length length(x) with the (first) index of y for each geometry 
-#' (point, grid cell centre, polygon or lines) in x. If y has attribute data, attribute 
-#' data are returned in a data.frame with number of rows equal to length(x)
+#' @param retList logical; see value
+#' @param fun optional, a function; see value
+#' @return If y is only geometry an object of length length(x). If returnList is 
+#' FALSE, a vector with the (first) index of y for each geometry (point, grid cell 
+#' centre, polygon or lines) in x. if returnList is TRUE, a list of length 
+#' length(x), with list element i the vector of all indices of the geometries in
+#'  y that correspond to the $i$-th geometry in x.
+#' If y has attribute data, attribute data are returned. returnList is FALSE, a
+#'  data.frame with number of rows equal to length(x) is returned, if it is TRUE a
+#'   list with length(x) elements is returned, with a list element the data.frame
+#'    elements of all geometries in y that correspond to that element of x.
 #' @author Bishop, T.
 #' @export
 #' 
 
-overDS <- function(x,y){
+overDS <- function(x, y, retList = FALSE, fun = NULL){
   
   require(sp)
   require(rgeos)
   
-  temp <- over(x,y,fn=length)
+  temp <- over(x, y, returnList=retList, fn=fun)
   return(temp)
   
 }
